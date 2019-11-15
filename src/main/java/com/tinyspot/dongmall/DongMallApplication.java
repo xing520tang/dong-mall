88889c219e56219e183bf35f8b40b0f8bd6d2b8c
tiny_spot <xing520tang@gmail.com>
2019-11-15 15:11:57 +0800

    初次提交

---
+++ src/main/java/com/tinyspot/dongmall/DongMallApplication.java
@@ -0,0 +1,13 @@
+package com.tinyspot.dongmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DongMallApplication.class, args);
+    }
+
+}
